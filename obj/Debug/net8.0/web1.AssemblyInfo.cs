55074b122b95deb655ef29c0d03550af2fbe9cd4
maIihu <nguyenmanhhung20184@gmail.com>
2024-10-21 00:55:09 +0700

    push push

--- obj/Debug/net8.0/web1.AssemblyInfo.cs
+++ obj/Debug/net8.0/web1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f654bad2f88e5571129651628fa8c7008c6b536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171a7a4dfcbb639503fc240386294ec134dae82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("web1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
