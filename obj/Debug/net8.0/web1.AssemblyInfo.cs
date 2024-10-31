4bf19e0a9f25e5177d48c1c0a0109f386387a668
maIihu <nguyenmanhhung20184@gmail.com>
2024-11-01 00:45:42 +0700

    Add: GopY, QuanLyDoanhThu, QuanLyYKien

--- obj/Debug/net8.0/web1.AssemblyInfo.cs
+++ obj/Debug/net8.0/web1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d385f0a5dc8e93ae45122e81d9f2c889426cd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42e7d6e81c7887d2d8be8070574d06bbaef3466")]
 [assembly: System.Reflection.AssemblyProductAttribute("web1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
